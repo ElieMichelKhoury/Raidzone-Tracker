04ea5ea33137825e42d964ad7c150a1d3090e811
ElieMichelKhoury <elie.michel.khoury@gmail.com>
2026-05-14 13:25:55 +0300

    Delete app directory

--- app/api/auth/signout/route.js
+++
@@ -1,8 +0,0 @@
-// app/api/auth/signout/route.js
-import { NextResponse } from 'next/server';
-import { destroySession } from '@/lib/session';
-
-export async function POST() {
-  await destroySession();
-  return NextResponse.redirect(process.env.NEXTAUTH_URL + '/');
-}
