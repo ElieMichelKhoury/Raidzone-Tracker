45798bb3d1b93091337bcb130359ad2805eaa96e
ElieMichelKhoury <elie.michel.khoury@gmail.com>
2026-05-14 13:26:55 +0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['avatars.steamstatic.com', 'avatars.akamai.steamstatic.com'],
-  },
-};
-
-module.exports = nextConfig;
